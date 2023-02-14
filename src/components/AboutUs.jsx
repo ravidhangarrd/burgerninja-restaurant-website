a279d4a90ded7ba8d8a8ba32068bf78f7f32220c
ravidhangarrd <ravidhangar0011@gmail.com>
2023-02-14 18:29:43 +0530

    about us section is add

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default function AboutUs() {
+const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <>
+    <section className='aboutus-section-container'>
+      <div className='aboutus-sec-left-con'>
+
+      </div>
+      <div className='aboutus-sec-right-con'>
+        <p id="about-text">About Us</p>
+        <h3>We speak the good<br/>food language</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquam nemo odit laborum quia praesentium tenetur ullam, modi quam rem qui ipsum voluptas, sapiente enim ad minima doloribus aperiam. Reiciendis.
+        </p>
+        <button>Learn More</button>
+      </div>
+    </section>
+    </>
   )
 }
+
+export default AboutUs
--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import TopBanner from './TopBanner'
+import AboutUs from './AboutUs'
 
 const MainBody = () => {
   return (
     <>
     <main>
         <TopBanner />
+        <AboutUs />
     </main>
     </>
   )
